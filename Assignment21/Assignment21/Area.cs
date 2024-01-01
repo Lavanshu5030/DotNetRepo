0775f1b7b899a84635c9856bad719d66c5990ade
lavanshu5030 <lavanshu2001@gmail.com>
2024-01-01 23:06:34 +0530

    Area of shapes Menu Driven Assignment21

---
+++ Assignment21/Assignment21/Area.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment21
+{
+    internal class Area
+    {
+        int m_side;
+        int m_length;
+        int m_width;
+        double m_radius;
+
+        //Constructor
+        internal Area(int side) { m_side = side; }
+        internal Area(int length, int width) { m_length = length; m_width = width; }
+
+        internal Area(double radius) {
+            m_radius = radius; 
+        }
+
+        internal int AreaOfSquare()
+        {
+            int area = m_side * m_side;
+            return area;
+        }
+        internal int AreaOfRectangle()
+        {
+            int area = m_length * m_width;
+            return area;
+        }
+
+        internal double AreaOfCircle()
+        {
+            double area = 3.14 * m_radius * m_radius;
+            return area;
+        }
+
+        
+    }
+}
---
+++ Assignment21/Assignment21/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice = 0;
+
+            do { 
+                Console.WriteLine("---Menu---");
+                Console.WriteLine("Enter 1 to get Square area");
+                Console.WriteLine("Enter 2 to get Rectangle area");
+                Console.WriteLine("Enter 3 to get Circle area");
+                Console.WriteLine("Enter 4 to exit area");
+                Console.WriteLine("Enter your choice:");
+                choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Area area1 = new Area(5);
+                        Console.WriteLine(area1.AreaOfSquare());
+                        break;
+
+                    case 2:
+                        Area area2 = new Area(2, 3);
+                        Console.WriteLine(area2.AreaOfRectangle()); 
+                        break;
+
+                    case 3:
+                        Area area3 = new Area(3.0);
+                        Console.WriteLine(area3.AreaOfCircle());
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice");
+                        break;
+                }
+            }
+            while (choice != 4);
+
+        }
+    }
+}
